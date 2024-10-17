5386c0608f58f1eb5337d694eb1a34ef57ad38a2
daniel <dpenedo@tutanota.com>
2024-10-17 20:58:54 -0300

    css module de clock

--- react-ts/src/components/Clock.tsx
+++ react-ts/src/components/Clock.tsx
@@ -1,4 +1,5 @@
 import { formatTime } from "../utils/timeUtils";
+import styles from "./clock.module.css";
 
 interface ClockProps {
     hours: number;
@@ -17,22 +18,20 @@ export default function Clock({hours, minutes, seconds, centiseconds, showCentis
         centiseconds: centiseconds!==undefined ? formatTime(centiseconds) : "00",
     };
   return (
-        <div className="clock">
-            <div className="clock__container">
-                <div className="clock__hours">{formattedTime.hours}</div>
-                <div className="clock__separator">:</div>
-                <div className="clock__min">{formattedTime.minutes}</div>
-                <div className="clock__separator">:</div>
-                <div className="clock__sec">{formattedTime.seconds}</div>
+            <div className={styles.clock}>
+                <div >{formattedTime.hours}</div>
+                <div className={styles.clock__separator}>:</div>
+                <div >{formattedTime.minutes}</div>
+                <div className={styles.clock__separator}>:</div>
+                <div >{formattedTime.seconds}</div>
                 { showCentiseconds && (
                     <>
-                <div className="clock__separator">:</div>
-                <div className="clock__centisec">{formattedTime.centiseconds}</div>
+                <div className={styles.clock__separator}>:</div>
+                <div >{formattedTime.centiseconds}</div>
                     </>
                 )
                 }
             </div>
-        </div>
   )
 }
 
